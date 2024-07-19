baf3fceecc1b3326fb5c0c39ebe35c50fa24fe37
cornel05 <ngotrandinhduy2019@gmail.com>
2024-07-20 02:13:11 +0700

    Change font property

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggleButton = document.createElement('button');
-    themeToggleButton.textContent = 'Toggle Theme';
-    themeToggleButton.className = 'theme-toggle-btn'; // Add a class name to the button
-    document.body.insertBefore(themeToggleButton, document.body.firstChild);
-
-    themeToggleButton.addEventListener('click', () => {
-        if (document.body.style.backgroundColor === 'black') {
-            document.body.style.backgroundColor = '#f4f4f4';
-            document.body.style.color = '#333';
-            themeToggleButton.textContent = 'Toggle Theme';
-        } else {
-            document.body.style.backgroundColor = 'black';
-            document.body.style.color = 'white';
-            themeToggleButton.textContent = 'Toggle Theme';
-        }
-    });
+document.querySelectorAll('a').forEach(function(link) {
+    link.setAttribute('target', '_blank');
 });
